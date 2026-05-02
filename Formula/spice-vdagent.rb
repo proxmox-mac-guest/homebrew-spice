3c6b787b336232bdd0c95a04b8993bead94da12a
Thomas Maerz <7740810+thomasmaerz@users.noreply.github.com>
2026-05-01 17:10:04 -0700

    fix: correct bottle block indentation

--- Formula/spice-vdagent.rb
+++ Formula/spice-vdagent.rb
@@ -8,12 +8,12 @@ class SpiceVdagent < Formula
   depends_on "glib" => :build
   depends_on "pkg-config" => :build
 
-bottle do
-  root_url "https://github.com/proxmox-mac-guest/spice-vdagent/releases/download/spice-vdagent-0.22.1-1"
-  sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0149fb71c187190584f84c248148389ea66085b7f5d0787b01911eb0fc406eb"
-  sha256 cellar: :any_skip_relocation, arm64_sonoma: "283b6ce34652ca861fd91e41b3315cc9cb0ee6d339a50a3d54b1277a246265b8"
-  sha256 cellar: :any_skip_relocation, arm64_tahoe: "5545a06c778e06f94dcb8a1f6ee422fb43c256acfafa789f14ade1f03641e110"
-  sha256 cellar: :any_skip_relocation, sequoia: "6827417e05b070c3816047c54f232aadf1b698141a8454470b90096c410fe2e7"
-  sha256 cellar: :any_skip_relocation, sonoma: "187eade16133b87044b029a8c1a9c2461d643a82248c0da4c9b843fd6dd46c6d"
-  sha256 cellar: :any_skip_relocation, tahoe: "9c255004c8ee3f6f931f1831635f959fc7da00fb560f1dc517fcd08bde54d349"
-end
+  bottle do
+    root_url "https://github.com/proxmox-mac-guest/spice-vdagent/releases/download/spice-vdagent-0.22.1-1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0149fb71c187190584f84c248148389ea66085b7f5d0787b01911eb0fc406eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "283b6ce34652ca861fd91e41b3315cc9cb0ee6d339a50a3d54b1277a246265b8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "5545a06c778e06f94dcb8a1f6ee422fb43c256acfafa789f14ade1f03641e110"
+    sha256 cellar: :any_skip_relocation, sequoia: "6827417e05b070c3816047c54f232aadf1b698141a8454470b90096c410fe2e7"
+    sha256 cellar: :any_skip_relocation, sonoma: "187eade16133b87044b029a8c1a9c2461d643a82248c0da4c9b843fd6dd46c6d"
+    sha256 cellar: :any_skip_relocation, tahoe: "9c255004c8ee3f6f931f1831635f959fc7da00fb560f1dc517fcd08bde54d349"
+  end
